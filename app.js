1093983bd4a3e2fd374afb586a2ca9a8fd4dc795
Bage Gonz <BageGonz@users.noreply.github.com>
2021-06-21 16:44:41 -0400

    inquirer

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2);
+inquirer 
+ .prompt([
+     {
+         type: 'input',
+         name: 'name',
+         message: 'what is your name?'
+     }
+ ])
+ .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Checkout index.html to see the output')
-})
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error (err);
+
+//     console.log('Portfolio complete! Checkout index.html to see the output')
+// })
+
+
 
 
 
